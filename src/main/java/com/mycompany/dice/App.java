754a10f4a121d28a801bb63308a49fe788c8b51a
Colin But <colinbut@users.noreply.github.com>
2016-04-25 09:03:13 +0100

    initial project structure by maven

---
+++ src/main/java/com/mycompany/dice/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.dice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
