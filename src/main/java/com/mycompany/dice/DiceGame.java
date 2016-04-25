124e0013ca9957b9252f75ac65350efb2bb80ff1
Colin But <colinbut@users.noreply.github.com>
2016-04-25 09:07:09 +0100

    removed generated code

---
+++ src/main/java/com/mycompany/dice/DiceGame.java
@@ -0,0 +1,18 @@
+package com.mycompany.dice;
+
+/**
+ * Dice
+ *
+ * @author colin
+ */
+public class DiceGame {
+
+    /**
+     * Main program
+     *
+     * @param args
+     */
+    public static void main( String[] args ) {
+        System.out.println("Dice");
+    }
+}
