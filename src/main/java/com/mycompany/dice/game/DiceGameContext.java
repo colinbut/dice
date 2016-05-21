0465b6065aca93d9b824e0588fd1b23bded425f7
Colin But <colinbut@users.noreply.github.com>
2016-05-21 13:42:58 +0100

    separation of game context from game play

---
+++ src/main/java/com/mycompany/dice/game/DiceGameContext.java
@@ -0,0 +1,116 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.dice.game;
+
+import com.mycompany.dice.Dice;
+import com.mycompany.dice.Player;
+import com.mycompany.dice.game.states.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Contains contextual management information associated to the game
+ *
+ * Is responsible for managing most game related activities
+ *
+ * @author colin
+ */
+public final class DiceGameContext {
+
+    private static final Logger logger = LoggerFactory.getLogger(DiceGameContext.class);
+
+    private GameState gameState;
+
+    private List<Player> players = new ArrayList<>();
+
+    private static Map<GameStates, GameState> gameStates = new EnumMap<>(GameStates.class);
+
+    static {
+        gameStates.put(GameStates.STARTED, StartedState.getInstance());
+        gameStates.put(GameStates.PLAYING, PlayingState.getInstance());
+        gameStates.put(GameStates.ENDED, EndingState.getInstance());
+    }
+
+
+    /**
+     * Starts the game
+     */
+    public void playGame() {
+        logger.info("Starting Dice game");
+
+        gameState = gameStates.get(GameStates.STARTED);
+        gameState.transition();
+
+        prepareGame();
+
+        gameState = gameStates.get(GameStates.PLAYING);
+        gameState.transition();
+
+        List<Thread> playerThreads = new ArrayList<>();
+        play(playerThreads);
+
+        // wait for this player thread to finish
+        for (Thread playerThread : playerThreads) {
+
+            try {
+                playerThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // when it gets here - game finished
+        gameState = gameStates.get(GameStates.ENDED);
+        gameState.transition();
+
+    }
+
+
+    /**
+     * Starts the player threads in order to start 'playing' the
+     * game
+     *
+     * @param playerThreads
+     */
+    private void play(List<Thread> playerThreads) {
+        for (Player player : players) {
+            Thread playerThread = new Thread(player);
+            playerThread.start();
+
+            logger.trace("Started player thread");
+
+            playerThreads.add(playerThread);
+
+        }
+    }
+
+
+    /**
+     * Prepares the game variables:
+     * - the Dice
+     * - the players
+     */
+    private void prepareGame() {
+        Dice dice = new Dice();
+        logger.trace("Initialised Dice object {}", dice);
+
+        Player player1 = new Player(dice, "Joe", "Jane");
+        Player player2 = new Player(dice, "Jane", "Joe");
+
+        logger.trace("Initialised Player 1 {}", player1);
+        logger.trace("Initialised Player 2 {}", player2);
+
+        dice.setWhoStarts(player1.getCurrentPlayer()); //let's make player 1 start the game
+
+        players.add(player1);
+        players.add(player2);
+    }
+}
